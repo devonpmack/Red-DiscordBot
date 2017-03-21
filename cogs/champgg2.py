b1b75ccfe50b6b6ca0e80d7109082ad7b9da05a6
SwissKnockoff <jpaol1@ocdsb.ca>
2017-03-21 16:44:19 -0400

    formatting fixes for input

--- cogs/champgg2.py
+++ cogs/champgg2.py
@@ -17,13 +17,38 @@ class champgg2:
         items = []
         say = ""
 
+        role = role.lower()
+        champ = champ.replace(' ', '')
+
+        shorthand = {
+
+            'mid':'middle',
+            'bot':'adc',
+            'jg':'jungle',
+            'sup':'support',
+            'supp':'support'
+
+        }
+
+        if
+
+        if role in shorthand.keys():
+            role = shorthand[role]
+
         request = requests.get(url)
         data = request.json()
 
+        roleNum = -1
+
         for i in range(0, len(data)):
-            if data[i]['role'] == role:
+            if data[i]['role'].lower() == role:
                 roleNum = i
                 break
+
+        if roleNum < 0:
+            await self.bot.say("Couldn't find that champion/role combination.")
+
+        await self.bot.say("Build winrate: " + str(data[roleNum]['items']['highestWinPercent']['winPercent']) + "\% \over " + str(data[roleNum]['items']['highestWinPercent']['winPercent']))
 
         for i in range(0, 6):
              items.append(str(data[roleNum]['items']['highestWinPercent']['items'][i]['name']))
