0e1fcfc1ad985e808e03b46bb964e504c62f1bba
SwissKnockoff <jpaol1@ocdsb.ca>
2017-03-22 16:52:57 -0400

    [i]

--- cogs/champgg2.py
+++ cogs/champgg2.py
@@ -40,10 +40,10 @@ class champgg2:
             await self.bot.say("Top 5 Champions for " + role.upper())
 
             for i in range(0, 5):
-                champ = str(data['data']['name'])
-                winrate = str(data['data']['general']['winPercent'])
-                playrate = str(data['data']['general']['playPercent'])
-                banrate = str(data['data']['general']['banRate'])
+                champ = str(data[i]['data']['name'])
+                winrate = str(data[i]['data']['general']['winPercent'])
+                playrate = str(data[i]['data']['general']['playPercent'])
+                banrate = str(data[i]['data']['general']['banRate'])
 
                 await self.bot.say(str(i + 1) + ". " + champ + " - Winrate: " + winrate + "%, Playrate: " + playrate + "%, Banrate " + banrate + "%.")
 
