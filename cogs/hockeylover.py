75cdc7436778f0ae1b4ee098171d978e8e3631e5
devonpmack <devonpmack@gmail.com>
2017-05-16 18:41:08 -0400

    fixed time

--- cogs/hockeylover.py
+++ cogs/hockeylover.py
@@ -18,7 +18,7 @@ class Hockeylover:
         """Tells you the time (in PJT)"""
         import datetime
         now = datetime.datetime.now()
-        eleven = now.replace(hour=23, minute=0, second=0, microsecond=0)
+        eleven = now.replace(hour=19, minute=0, second=0, microsecond=0)
         diff = eleven-now
         if diff.total_seconds() > 0:
             await self.bot.say("The current time is %s UJT(Until Justin's Bedtime)" % ':'.join(str(diff).split(':')[:2]))
