0e50ced683a59ef2e3c107237b814a51970f2de1
devonpmack <devonpmack@gmail.com>
2018-02-04 21:44:43 -0500

    greetings

--- cogs/memes.py
+++ cogs/memes.py
@@ -34,9 +34,9 @@ class Memes:
         await self.bot.say("GREETINGS!!!!!!!!!!!!!!!!!!!!!!!!!")
         try:
             await self.bot.kick(ctx.message.author)
-            await self.bot.say(ctx.message.author.mention + " has been greeted. Let this be a warning to you all.")
+            await self.bot.say  (ctx.message.author.mention + " has been greeted. Let this be a warning to you all.")
         except discord.errors.Forbidden:
-            await self.bot.say("(this person is too powerful)")
+            await self.bot.say("(this person is powerful)")
 
     @commands.command()
     async def dank(self):
